fcc61542c2da2cb51db9bd5dcc0ef52032b6e179
JamilHans <jamilhfc16@gmail.com>
2023-05-23 21:35:04 -0500

    Change module domain at model, repositories, resource and use case files.

--- domain/src/main/java/com/trainingandroid/domain/usecase/GetDetailMovieUseCaseImpl.kt
+++ domain/src/main/java/com/trainingandroid/domain/usecase/GetDetailMovieUseCaseImpl.kt
@@ -2,8 +2,10 @@ package com.trainingandroid.domain.usecase
 
 import com.trainingandroid.domain.repositories.MoviesRepository
 
+
 class GetDetailMovieUseCaseImpl(
     private val moviesRepository: MoviesRepository
 ) : GetDetailMovieUseCase {
+
     override suspend operator fun invoke(id: Int) = moviesRepository.getDetailMovie(id)
 }
