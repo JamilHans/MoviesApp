fbea42f6f299ff82de30932d00f1ca548fc2576f
JamilHans <jamilhfc16@gmail.com>
2023-05-23 21:38:48 -0500

    Change module data at api, datasource and repositories files

--- data/src/main/java/com/trainingandroid/data/api/RemoteService.kt
+++ data/src/main/java/com/trainingandroid/data/api/RemoteService.kt
@@ -3,17 +3,17 @@ package com.trainingandroid.data.api
 import com.trainingandroid.data.model.movie.MovieResponse
 import com.trainingandroid.data.model.movie.WrappedListResponse
 import com.trainingandroid.data.model.moviedetail.MovieDetailResponse
-import retrofit2.Response
+import com.trainingandroid.domain.model.error.Error
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface RemoteService {
     @GET("upcoming?api_key=a270c034df12879853e01114c35c4292")
-    suspend fun getUpcoming(): Response<WrappedListResponse<MovieResponse>>
+    suspend fun getUpcoming(): NetworkResponse<WrappedListResponse<MovieResponse>, Error>
 
     @GET("popular?api_key=a270c034df12879853e01114c35c4292")
-    suspend fun getPopular(): Response<WrappedListResponse<MovieResponse>>
+    suspend fun getPopular(): NetworkResponse<WrappedListResponse<MovieResponse>, Error>
 
     @GET("{keyword_id}?api_key=a270c034df12879853e01114c35c4292")
-    suspend fun getMovieDetail(@Path("keyword_id") id: Int): Response<MovieDetailResponse>
+    suspend fun getMovieDetail(@Path("keyword_id") id: Int): NetworkResponse<MovieDetailResponse, Error>
 }
