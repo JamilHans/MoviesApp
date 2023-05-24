b6c247c2141a342357d0f42696d30040cd683383
JamilHans <jamilhfc16@gmail.com>
2023-05-23 21:36:33 -0500

    Change module data at datasource and repositories tests

--- data/src/test/java/com/trainingandroid/data/datasource/MoviesRemoteDataSourceImpTest.kt
+++ data/src/test/java/com/trainingandroid/data/datasource/MoviesRemoteDataSourceImpTest.kt
@@ -1,11 +1,12 @@
 package com.trainingandroid.data.datasource
 
+import com.trainingandroid.data.api.NetworkResponse
 import com.trainingandroid.data.api.RemoteService
-import com.trainingandroid.data.mappers.toDomainModel
 import com.trainingandroid.data.model.movie.Dates
 import com.trainingandroid.data.model.movie.MovieResponse
 import com.trainingandroid.data.model.movie.WrappedListResponse
 import com.trainingandroid.data.model.moviedetail.MovieDetailResponse
+import com.trainingandroid.domain.model.error.Error
 import com.trainingandroid.domain.resource.ResultType
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
@@ -15,23 +16,44 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.whenever
-import retrofit2.Response
 
 @RunWith(MockitoJUnitRunner::class)
 class MoviesRemoteDataSourceImpTest {
     @Mock
     lateinit var remoteService: RemoteService
-    private var listMovies: Response<WrappedListResponse<MovieResponse>> =
-        Response.success(
-            WrappedListResponse(
-                Dates("1", "1"), 1, emptyList(), 1, 2
-            )
+    private var listMoviesResponse: WrappedListResponse<MovieResponse> =
+        WrappedListResponse(
+            Dates("1", "1"), 1, emptyList(), 1, 2
         )
 
-    private var detailMovie: Response<MovieDetailResponse> = Response.success(
-        null
-    )
-
+    private var detailMovie: MovieDetailResponse = (
+            MovieDetailResponse(
+                true,
+                "a",
+                1,
+                emptyList(),
+                "",
+                1,
+                "",
+                "",
+                "",
+                "",
+                0.0,
+                "true",
+                emptyList(),
+                emptyList(),
+                "",
+                1,
+                1,
+                emptyList(),
+                "",
+                "",
+                "",
+                true,
+                0.0,
+                1,
+            )
+            )
 
     private lateinit var sut: MoviesRemoteDataSource
 
@@ -43,12 +65,12 @@ class MoviesRemoteDataSourceImpTest {
     @Test
     fun `Getting upcoming movie should return upcoming movie when return has success`() {
         runBlocking {
-            val successResultType =
-                ResultType.Success(data = listMovies.body()?.results?.map { it.toDomainModel() })
+            val apiResult = NetworkResponse.Success(listMoviesResponse)
+            val successResultType = ResultType.Success<List<MovieResponse>, Error>(emptyList())
             whenever(
                 remoteService.getUpcoming()
             ).thenReturn(
-                listMovies
+                apiResult
             )
             val result = sut.getUpcomingMovies()
             assertEquals(result, successResultType)
@@ -58,12 +80,13 @@ class MoviesRemoteDataSourceImpTest {
     @Test
     fun `Getting populate movie should return populate movie when return has success`() {
         runBlocking {
+            val apiResult = NetworkResponse.Success(listMoviesResponse)
             val successResultType =
-                ResultType.Success(data = listMovies.body()?.results?.map { it.toDomainModel() })
+                ResultType.Success<List<MovieResponse>, Error>(emptyList())
             whenever(
                 remoteService.getPopular()
             ).thenReturn(
-                listMovies
+                apiResult
             )
             val result = sut.getPopulateMovies()
             assertEquals(result, successResultType)
@@ -73,12 +96,13 @@ class MoviesRemoteDataSourceImpTest {
     @Test
     fun `Getting detail movie should return detail movie when return has success`() {
         runBlocking {
+            val apiResult = NetworkResponse.Success(detailMovie)
             val successResultType =
-                ResultType.Success(data = detailMovie.body()?.toDomainModel())
+                ResultType.Success<MovieDetailResponse, Error>(detailMovie)
             whenever(
                 remoteService.getMovieDetail(1)
             ).thenReturn(
-                detailMovie
+                apiResult
             )
             val result = sut.getDetailMovie(1)
             assertEquals(result, successResultType)
