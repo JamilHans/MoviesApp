dd02283c702dc21996cb8c442a20be40a7c2d20c
JamilHans <jamilhfc16@gmail.com>
2023-06-08 16:39:50 -0500

    refactor: reformat code toResultType

--- data/src/main/java/com/trainingandroid/data/datasource/MovieDetailRemoteDataSourceImp.kt
+++ data/src/main/java/com/trainingandroid/data/datasource/MovieDetailRemoteDataSourceImp.kt
@@ -2,13 +2,14 @@ package com.trainingandroid.data.datasource
 
 import com.trainingandroid.data.api.RemoteService
 import com.trainingandroid.data.model.moviedetail.MovieDetailResponse
-import com.trainingandroid.data.util.convertToResultType
+import com.trainingandroid.data.util.toResultType
 import com.trainingandroid.domain.model.error.Error
 import com.trainingandroid.domain.resource.ResultType
 
-class MovieDetailRemoteDataSourceImp(private val remoteService: RemoteService) : MovieDetailRemoteDataSource {
+class MovieDetailRemoteDataSourceImp(private val remoteService: RemoteService) :
+    MovieDetailRemoteDataSource {
     override suspend fun getDetailMovie(id: Int): ResultType<MovieDetailResponse, Error> {
         val response = remoteService.getMovieDetail(id)
-        return response.convertToResultType()
+        return response.toResultType()
     }
 }
