58ad06fc27a0f97fe0d4df06a5995a7e222f571d
JamilHans <jamilhfc16@gmail.com>
2023-06-07 22:18:18 -0500

    refactor: convert data class to sealed class

--- app/src/main/java/com/trainingandroid/mobiedbapp/presentation/home/HomeState.kt
+++ app/src/main/java/com/trainingandroid/mobiedbapp/presentation/home/HomeState.kt
@@ -3,15 +3,15 @@ package com.trainingandroid.mobiedbapp.presentation.home
 import com.trainingandroid.domain.model.movie.Movies
 
 sealed class HomeState {
-    data class UpComingMoviesState(
-        val isLoading: Boolean = false,
-        val error: String? = null,
-        val upComingMovies: List<Movies>? = null
-    ) : HomeState()
+    sealed class UpComingMoviesState : HomeState() {
+        data class ShowLoading(val isLoading: Boolean) : UpComingMoviesState()
+        data class Error(val message: String) : UpComingMoviesState()
+        data class ShowUpcomingMovies(val upComingMovies: List<Movies>) : UpComingMoviesState()
+    }
 
-    data class PopulateMoviesState(
-        val isLoading: Boolean = false,
-        val error: String? = null,
-        val populateMovies: List<Movies>? = null
-    ) : HomeState()
+    sealed class PopulateMoviesState : HomeState() {
+        data class ShowLoading(val isLoading: Boolean): PopulateMoviesState()
+        data class Error(val message: String): PopulateMoviesState()
+        data class ShowPopulateMovies(val populateMovies: List<Movies>): PopulateMoviesState()
+    }
 }
