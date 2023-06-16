3ccaa9a72ebd342f0ff848292e1f88b8cf009f51
JamilHans <jamilhfc16@gmail.com>
2023-06-16 11:52:39 -0500

    feature: add firebase config and MyFirebaseMessagingService

--- app/src/main/java/com/trainingandroid/mobiedbapp/presentation/MainHostActivity.kt
+++ app/src/main/java/com/trainingandroid/mobiedbapp/presentation/MainHostActivity.kt
@@ -1,7 +1,11 @@
 package com.trainingandroid.mobiedbapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.trainingandroid.mobiedbapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,5 +15,12 @@ class MainHostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+        })
     }
 }
